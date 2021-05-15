e7d360ebf72b7dc63cf0874f763fb3d68f4c189a
unknown <ngtbao2000@gmail.com>
2021-05-15 19:42:27 +0700

    room-basic

---
+++ app/src/main/java/com/example/roomdatabase/User.java
@@ -0,0 +1,13 @@
+package com.example.roomdatabase;
+
+    public class User {
+
+        @PrimaryKey
+        public int uid;
+
+        @ColumnInfo(name = "first_name")
+        public String firstName;
+
+        @ColumnInfo(name = "last_name")
+        public String lastName;
+}
